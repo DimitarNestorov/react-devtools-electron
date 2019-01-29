08d7ef8cda707bfde50a60b8bd0a7bfd24b3809a
Dimitar Nestorov <dimitarnestorov@users.noreply.github.com>
2019-01-29 23:36:39 +0200

    Check if extension is already installed

--- src/utils.ts
+++ src/utils.ts
@@ -3,10 +3,27 @@ import { join } from 'path'
 
 import { BrowserWindow } from 'electron'
 
+interface DevToolsExtensions {
+	[ name: string ]: { name: string, version: string }
+}
+
+interface ChromeExtensionManifest {
+	name: string
+	version: string
+}
+
 const extensionPath = join(__dirname, '..', 'extension')
+const extensionManifest: ChromeExtensionManifest = require(`${extensionPath}/manifest.json`)
 
 export function addReactDevToolsExtension() {
-	BrowserWindow.addDevToolsExtension(extensionPath)
+	const extensions = BrowserWindow.getDevToolsExtensions() as DevToolsExtensions
+	const installedExtension = extensions[extensionManifest.name] 
+	if (!installedExtension) {
+		BrowserWindow.addDevToolsExtension(extensionPath)
+	} else if (installedExtension.version !== extensionManifest.version) {
+		removeReactDevToolsExtension()
+		BrowserWindow.addDevToolsExtension(extensionPath)
+	}
 }
 
 export function removeReactDevToolsExtension() {
