102e7e6e2e550010144b9152022cdf2757eeaf52
Dimitar Nestorov <dimitarnestorov@users.noreply.github.com>
2019-08-16 13:48:49 +0300

    Updating react-devtools

--- example/src/renderer.tsx
+++ example/src/renderer.tsx
@@ -1,9 +1,21 @@
-import { createElement } from 'react'
+import { createElement, useState, Fragment } from 'react'
 import { render } from 'react-dom'
 
+function Counter() {
+	const [current, setCurrent] = useState(0)
+	return <button onClick={() => setCurrent(current + 1)}>{current}</button>
+}
+
+function App() {
+	return <Fragment>
+		<div>
+			We are using Node.js {process.versions.node}, Chromium {process.versions.chrome}, and Electron {process.versions.electron}
+		</div>
+		<Counter />
+	</Fragment>
+}
+
 render(
-	<div>
-		We are using Node.js {process.versions.node}, Chromium {process.versions.chrome}, and Electron {process.versions.electron}
-	</div>,
-	document.getElementById('root')
+	<App />,
+	document.getElementById('root'),
 )
